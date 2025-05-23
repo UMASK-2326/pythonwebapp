4ab2a7102a7589b1f04c65def2fcb0023ec09634
uma haribabu <umaharibabu2003@gmail.com>
2025-05-23 15:18:28 +0530

    2305

--- src_new/app.py
+++ src_new/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_docker():
-    return '<h1>Hi Uma Santhosh</h1><br><p>Have a nice day!</p> '
+    return '<h1>HAPPY BIRTHDAY MY LOVE!</h1><br><p>-UmaSanthosh!</p> '
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
